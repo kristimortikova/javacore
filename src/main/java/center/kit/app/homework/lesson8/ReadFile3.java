292514862b1990a14e073bd756957d7908ab06a1
kristimortikova <kristimortikova@gmail.com>
2017-12-23 11:48:37 +0300

    [WEB-] lesson8

---
+++ src/main/java/center/kit/app/homework/lesson8/ReadFile3.java
@@ -0,0 +1,48 @@
+package center.kit.app.homework.lesson8;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadFile3 {
+    public static void readFile(String path)
+            throws IOException {
+        boolean check = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter name file .txt");
+
+        String nameFile = scanner.next();
+        String currentDir = System.getProperty("user.dir");
+        String windowsStyle = "dir\\" + nameFile;
+        File file = new File(currentDir, windowsStyle);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter
+                (new FileOutputStream("C:\\kit\\javacore\\" + nameFile)));
+        System.out.println(file.getCanonicalPath());
+        String line;
+        while (check) {
+            line = scanner.next();
+            switch (line) {
+                case "exit":
+                    writer.close();
+                    check = false;
+                    break;
+                default:
+                    writer.append(line);
+                    writer.newLine();
+                    break;
+            }
+
+        }
+        writer.close();
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/center/kit/runners/homework/lesson8/ReadFile3Runner.java
@@ -0,0 +1,11 @@
+package center.kit.runners.homework.lesson8;
+
+import center.kit.app.homework.lesson8.ReadFile3;
+
+import java.io.IOException;
+
+public class ReadFile3Runner {
+    public static void main(String[] args) throws IOException {
+        ReadFile3.readFile("C:\\kit\\javacore\\output.txt");
+    }
+}
